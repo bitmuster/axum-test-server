06389b120e6497e460a359dbcd4c3a1914cfcf96
Michael Abel <mabel@bitmuster.org>
2025-07-08 22:07:54 +0200

    Update script

--- blend.py
+++ blend.py
@@ -5,27 +5,31 @@ import requests
 server = "localhost"
 url = f"https://{server}:44001/swagger-ui/"
 
-url_up = f"https://{server}:44001/api/v1/todos/upload/"
-url_list = f"https://{server}:44001/api/v1/todos/list"
-url_blend = f"https://{server}:44001/api/v1/todos/blend"
+url_up = f"https://{server}:44001/api/v1/blend/upload/"
+url_list = f"https://{server}:44001/api/v1/blend/list"
+url_blend = f"https://{server}:44001/api/v1/blend/blend"
 
 file_list = [
     "../BlendResult/robot/results/output_a.xml",
     "../BlendResult/robot/results/output_b.xml",
-    "../BlendResult/robot/results/output_c.xml",
+    "../BlendResult/robot/results/output_c_fail.xml",
 ]
+
+headers = {'theapikey': 'rocks'}
+
+verify=False
 
 for file in file_list:
     filename = os.path.basename(file)
     files = {'upload_file': open(file, 'rb')}
-    r = requests.post(url_up+filename, files=files, verify=False)
+    r = requests.post(url_up+filename, files=files, verify=verify, headers=headers)
     r.raise_for_status()
 
-ret = requests.get(url_list, verify=False)
+ret = requests.get(url_list, verify=verify, headers=headers)
 ret.raise_for_status()
 print(ret.text)
 
-ret = requests.get(url_blend, verify=False)
+ret = requests.get(url_blend, verify=verify, headers=headers)
 ret.raise_for_status()
 
 # print(ret.text)
