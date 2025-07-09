b166722499bce067fe3dcb6d3424e647a231f03a
Michael Abel <mabel@bitmuster.org>
2025-07-09 11:58:39 +0200

    extract variables

--- blend.py
+++ blend.py
@@ -1,13 +1,13 @@
-
 import os.path
 import requests
 
 server = "localhost"
-url = f"https://{server}:44001/swagger-ui/"
+port = 44001
 
-url_up = f"https://{server}:44001/api/v1/blend/upload/"
-url_list = f"https://{server}:44001/api/v1/blend/list"
-url_blend = f"https://{server}:44001/api/v1/blend/blend"
+# url = f"https://{server}:{port}/swagger-ui/"
+url_up = f"https://{server}:{port}/api/v1/blend/upload/"
+url_list = f"https://{server}:{port}/api/v1/blend/list"
+url_blend = f"https://{server}:{port}/api/v1/blend/blend"
 
 file_list = [
     "../BlendResult/robot/results/output_a.xml",
@@ -15,14 +15,14 @@ file_list = [
     "../BlendResult/robot/results/output_c_fail.xml",
 ]
 
-headers = {'theapikey': 'rocks'}
-
-verify=False
+headers = {"theapikey": "rocks"}
+verify = False
 
 for file in file_list:
     filename = os.path.basename(file)
-    files = {'upload_file': open(file, 'rb')}
-    r = requests.post(url_up+filename, files=files, verify=verify, headers=headers)
+    files = {"upload_file": open(file, "rb")}
+    r = requests.post(url_up + filename, files=files,
+                      verify=verify, headers=headers)
     r.raise_for_status()
 
 ret = requests.get(url_list, verify=verify, headers=headers)
@@ -35,4 +35,3 @@ ret.raise_for_status()
 # print(ret.text)
 with open("out.ods", "wb") as f:
     f.write(ret.content)
-
